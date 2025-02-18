dd1745f15ec8bfb79e1d0ce58a0bd6fbcbe5bb2d
24f1002537 <24f1002537@ds.study.iitm.ac.in>
2025-02-18 15:33:57 +0530

    Adding registration page

--- app.py
+++ app.py
@@ -9,5 +9,8 @@ def submit():
     if((request.form['username']=="prakhar@gmail.com")&(request.form['password']=="admin")):
         return render_template('Admin.html')
     return render_template('login.html',again=True)
+@app.route('/create')
+def create():
+    return render_template('create.html')
 if __name__ == '__main__':
     app.run(debug=True)
