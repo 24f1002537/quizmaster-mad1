760e3bc682c340f688215105e11db3b343d65926
24f1002537 <24f1002537@ds.study.iitm.ac.in>
2025-02-24 01:22:47 +0530

    Adding user in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template,url_for,request
-import sqlite3
+from database import *
+
 
 app = Flask(__name__)
 app.app_context().push()
@@ -10,10 +11,17 @@ def login():
 def submit():
     if((request.form['username']=="prakhar@gmail.com")&(request.form['password']=="admin")):
         return render_template('Admin.html')
+    elif(check(request.form['username'],request.form['password'])):
+        return render_template('user.html')
     return render_template('login.html',again=True)
 @app.route('/create')
 def create():
     return render_template('create.html')
-
+@app.route('/to_database',methods = ["Post"])
+def create_user():
+    if check(request.form['username'],request.form['password']):
+        return render_template('create.html',again = True)
+    create_student(request.form['username'],request.form['password'],request.form['full_name'],request.form['Qualification'],request.form['dob'])
+    return render_template('create.html',succes=True)
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,13 +1,31 @@
 import sqlite3
 conn = sqlite3.connect('database.sqlite3')
 cur = conn.cursor()
-conn.execute('''CREATE TABLE IF NOT EXISTS users (
+cur.execute('''CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username_email TEXT NOT NULL,
     password TEXT UNIQUE NOT NULL,
     full_name TEXT,
     qualification TEXT,
-    dob INTEGER
+    dob DATE
 )''')
 conn.commit()
 conn.close()
+def check(usrnm,psw):
+    con =sqlite3.connect('database.sqlite3')
+    cur = con.cursor()
+    query = f"SELECT id FROM users WHERE username_email=? AND password = ?"
+    cur.execute(query,(usrnm,psw))
+    res = cur.fetchall()
+    con.commit()
+    con.close()
+    if res:
+        return True
+    return False
+def create_student(usrnm,psw,full_name,qual,dob):
+    conn = sqlite3.connect('database.sqlite3')
+    cur = conn.cursor()
+    query = f"INSERT INTO users (username_email,password,full_name,qualification,dob) VALUES(?,?,?,?,?)"
+    cur.execute(query,(usrnm,psw,full_name,qual,dob))
+    conn.commit()
+    conn.close()
