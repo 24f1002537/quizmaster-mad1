3e617896ff30bb249cbd864e2d9a03d4888eec43
24f1002537 <24f1002537@ds.study.iitm.ac.in>
2025-02-24 02:34:49 +0530

    Adding user in database

--- database.py
+++ database.py
@@ -2,12 +2,44 @@ import sqlite3
 conn = sqlite3.connect('database.sqlite3')
 cur = conn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY ,
     username_email TEXT NOT NULL,
     password TEXT UNIQUE NOT NULL,
     full_name TEXT,
     qualification TEXT,
     dob DATE
+)''')
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS subject(
+    id INTEGER PRIMARY KEY ,
+    name TEXT,
+    description TEXT
+)''')
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS chapter(
+    id INTEGER PRIMARY KEY ,
+    name TEXT,
+    description TEXT
+)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS quiz(
+    id INTEGER PRIMARY KEY,
+    chapter_id INTEGER,
+    date_of_quiz DATE,
+    time_duration TIME,
+    remarks TEXT,
+    FOREIGN KEY(chapter_id) REFERENCES chapter(id)
+)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS question(
+    id PRIMARY KEY ,
+    quiz_id INTEGER,
+    question_statement TEXT,
+    option_1 TEXT,
+    option_2 TEXT,option_3 TEXT,option_4 TEXT,correct_option TEXT,FOREIGN KEY(quiz_id) REFERENCES quiz(id)
+)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS scores(
+            id PRIMARY KEY,
+            quiz_id INTEGER,user_id INTEGER,time_stamp_of_atempt TIME,total_scored INTEGER,
+            FOREIGN KEY(quiz_id) REFERENCES quiz(id),FOREIGN KEY(user_id) REFERENCES user(id)            
 )''')
 conn.commit()
 conn.close()
